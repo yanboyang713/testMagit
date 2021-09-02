ad7fbaa2bfb0646ce2e2afe0e3224bb33b660f8a
Boyang Yan <yanboyang713@gmail.com>
2021-09-02 20:39:08 +1000

    init cv

---
+++ forPHD/auto/cvEnglish.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook
+ "cvEnglish"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "friggeri-cv"
+    "friggeri-cv10"
+    "afterpage"
+    "hyperref"
+    "color"
+    "xcolor"
+    "enumitem")
+   (LaTeX-add-bibliographies
+    "bibliography")
+   (LaTeX-add-xcolor-definecolors
+    "myblue"
+    "pblue"))
+ :latex)
+
---
+++ forUVIC/auto/cvEnglish.el
@@ -0,0 +1,24 @@
+(TeX-add-style-hook
+ "cvEnglish"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "friggeri-cv"
+    "friggeri-cv10"
+    "afterpage"
+    "hyperref"
+    "color"
+    "xcolor"
+    "enumitem")
+   (LaTeX-add-xcolor-definecolors
+    "myblue"
+    "pblue"))
+ :latex)
+
